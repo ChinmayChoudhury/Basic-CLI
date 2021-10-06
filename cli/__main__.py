cb661d5dbfc2b39d3658c98fdfeeaea98f86afa1
Chinmay Nrusingh Choudhury <choudhurychinmay0@gmail.com>
2021-10-06 20:10:17 +0530

    some options

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,18 +1,27 @@
 import sys
 from .classmodule import MyClass
 from .funcmodule import my_function
-
+from .helpmodule import show_help
+from . import __version__
 def main():
     print("in main")
     args = sys.argv[1:]
-    print("count of args :: {}".format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+    print(args[0])
+    #help
+    if args[0] == "-h" or args[0] == "--help":
+        show_help()
+        exit()
+    elif args[0] == "-v" or args[0] == "--version":
+        print(f"chinmaycli v{__version__}")
+    else:
+        print("count of args :: {}".format(len(args)))
+        for arg in args:
+            print('passed argument :: {}'.format(arg))
     
-    my_function("hello world")
+    # my_function("fromfun_hello world")
 
-    my_obj = MyClass("Chinmay")
-    my_obj.say_name()
+    # my_obj = MyClass("from_obj")
+    # my_obj.say_name()
 
 
 if __name__ == '__main__':
---
+++ cli/helpmodule.py
@@ -0,0 +1,5 @@
+def show_help():
+    print("Usage: chinmaycli STRING1 [STRING2 STRING3 ...]")
+    print("Optional arguments:")
+    print("\t -h, --help\t to show this help message")
+    print("\t -v, --version\t to show version number")
